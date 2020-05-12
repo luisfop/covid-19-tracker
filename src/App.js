7ec3d57a6c584abd93bf8abdc03aa8f27d31cc98
luisop <luisfop77@gmail.com>
2020-05-12 00:39:36 -0300

    cleaned some commentaries

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
 
   
   render() {
-  
+    
     return (
       <div className={ styles.container}>
           <h1 className={styles.title}>COVID - 19</h1>
@@ -39,6 +39,7 @@ class App extends React.Component {
       </div>
     );
   }
+
 }
 
 export default App;
--- src/components/countryPicker/countryPicker.jsx
+++ src/components/countryPicker/countryPicker.jsx
@@ -14,22 +14,15 @@ const CountryPicker = (props) => {
              setFetchCountries( await fetchCountry());
         }
         fetchAPI();
-        
     },[setFetchCountries]);
-
-    
-    // console.log('fetchedCountries => ' ,fetchedCountries);
 
     return(
         <div>
-            {/* <h1 onClick={(e) => props.handleCountry('xablau => ', e)}>This is CountryPicker Component</h1> */}
             <FormControl>
                 <NativeSelect defaultValue="" onChange={(e) => props.handleCountry( e.target.value)}>
                     <option defaultValue="Global"> Global</option>
-                    {fetchedCountries.map((country, i) => <option value={country} key={i}>{country}</option>)}
-                    
+                    {fetchedCountries.map((country, i) => <option value={country} key={i}>{country}</option>)}       
                 </NativeSelect>
-
             </FormControl>
             
         </div>
