6fdd65481c8af3e867cb727bdd69501b8c0deaad
luisop <luisfop77@gmail.com>
2020-05-12 00:59:06 -0300

    cleaned countrypicker component

--- src/components/countryPicker/countryPicker.jsx
+++ src/components/countryPicker/countryPicker.jsx
@@ -18,19 +18,11 @@ const CountryPicker = (props) => {
     },[setFetchCountries]);
 
     return(
-<<<<<<< HEAD
-        <div>
-            <FormControl>
-                <NativeSelect defaultValue="" onChange={(e) => props.handleCountry( e.target.value)}>
-                    <option defaultValue="Global"> Global</option>
-                    {fetchedCountries.map((country, i) => <option value={country} key={i}>{country}</option>)}       
-=======
         <div className={styles.pickerContainer}>
             <FormControl>
                 <NativeSelect defaultValue="" className={cx(styles.optionPicker)} onChange={(e) => props.handleCountry( e.target.value)}>
                     <option defaultValue="Global">Global</option>
                     {fetchedCountries.map((country, i) => <option value={country} key={i}>{country}</option>)}
->>>>>>> tracker-05
                 </NativeSelect>
             </FormControl>
             
